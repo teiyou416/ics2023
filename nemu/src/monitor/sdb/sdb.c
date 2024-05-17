8d4b0eeaea54b1588cb7feacb86e6bc139d727e0
tracer-ics2023 <tracer@njuics.org>
2024-05-17 15:04:12 +0900

    >  compile NEMU 221220000 张三 Linux teiyou 6.5.0-28-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 28 23:46:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:04:12 up 2 days,  5:33,  2 users,  load average: 1.32, 1.39, 1.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -63,7 +63,7 @@ static int cmd_info(char *args) {
     }
     return 0;
 }
-/*static int cmd_x(char *args) {
+/* static int cmd_x(char *args) {
     if (args == NULL) {
         printf("wrong input!\n");
         return 0;
@@ -80,8 +80,22 @@ static int cmd_info(char *args) {
         addr += 4;
     }
     return 0;
-}*/
+} */
 static int cmd_x(char *args) {
+    char *n = strtok(args, " ");
+    char *baseaddr = strtok(NULL, " ");
+    int len = 0;
+    paddr_t addr = 0;
+    sscanf(n, "%d", &len);
+    sscanf(baseaddr, "%x", &addr);
+    for (int i = 0; i < len; i++) {
+        printf("%x\n", paddr_read(addr, 4)); // addr len
+        addr = addr + 4;
+    }
+    return 0;
+}
+
+/*  static int cmd_x(char *args) {
     if (args == NULL) {
         printf("Wrong Command!\n");
         return 0;
@@ -91,12 +105,10 @@ static int cmd_x(char *args) {
     sscanf(args, "%d%x", &N, &startAddress);
     for (int i = 0; i < N; i++) {
         printf("%x\n", paddr_read(startAddress, 4));
-        // C语言会自动执行类型提升以匹配表达式的操作数的类型。所以，4 被转换为
-        // uint32_t，
         startAddress += 4;
     }
     return 0;
-}
+} */
 
 static int cmd_c(char *args) {
     cpu_exec(-1);
