69d7c60337fe88da3ccb7e5ee29a2ad17ffbeef4
tracer-ics2023 <tracer@njuics.org>
2024-05-27 11:23:15 +0900

    >  compile NEMU 221220000 张三 Linux chengye 6.6.26-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Apr 10 20:11:08 UTC 2024 x86_64 GNU/Linux  11:23:15 up  2:30,  1 user,  load average: 0.52, 0.55, 0.57

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -178,7 +178,9 @@ word_t findMajor(word_t p, word_t q) {
 
         for (int i = 0; i < nr_token; i++) {
             if (tokens[i].type == '*' &&
-                (i == 0 || tokens[i + 1].type == '(')) {
+                (i == 0 || tokens[i - 1].type == '+' ||
+                 tokens[i - 1].type == '-' || tokens[i - 1].type == '*' ||
+                 tokens[i - 1].type == '/')) {
                 tokens[i].type = POINTER;
                 return i;
             }
