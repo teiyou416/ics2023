792f43267649334dcf246d42245f90e42e0433a4
teiyou <teiyou416@gmail.com>
2024-06-09 03:27:13 +0900

    Working on hello-str

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,12 +6,11 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static char sprint_buf[1024];
 int printf(const char *fmt, ...) {
-    va_list args; // 用于存放参数列表的数据结构
+    va_list args;
     int n;
-    /*根据最后一个fmt来初始化参数列表，至于为什么是最后一个参数，是与va_start有关。*/
     va_start(args, fmt);
     n = vsprintf(sprint_buf, fmt, args);
-    va_end(args); // 执行清理参数列表的工作
+    va_end(args);
     putstr(sprint_buf);
     return n;
 }
