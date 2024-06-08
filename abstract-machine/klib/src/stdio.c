6d9a1b878d1fb7fae7127248c29f80bad3179a33
tracer-ics2023 <tracer@njuics.org>
2024-06-09 02:45:36 +0900

    >  compile NEMU 221220000 张三 Linux teiyou 6.5.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 26 11:23:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:45:36 up 1 day, 11:58,  2 users,  load average: 1.43, 1.31, 1.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,33 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+static char sprint_buf[1024];
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    va_list args; // 用于存放参数列表的数据结构
+    int n;
+    /*根据最后一个fmt来初始化参数列表，至于为什么是最后一个参数，是与va_start有关。*/
+    va_start(args, fmt);
+    n = vsprintf(sprint_buf, fmt, args);
+    va_end(args); // 执行清理参数列表的工作
+    putstr(sprint_buf);
+    return n;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
-}
+int sprintf(char *out, const char *fmt, ...) { panic("Not implemented"); }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 #endif
