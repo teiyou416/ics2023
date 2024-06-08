fda6d8ebfdd31493a22789bb2d193f07fb7270b3
tracer-ics2023 <tracer@njuics.org>
2024-06-08 22:53:38 +0900

    >  compile NEMU 221220000 张三 Linux teiyou 6.5.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 26 11:23:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:53:38 up 1 day,  8:06,  2 users,  load average: 1.30, 1.42, 1.36

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -92,10 +92,10 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
         src2R();
         immB();
         break;
-    case TYPE_R:
-        src1R();
-        src2R();
-        break;
+        // case TYPE_R:
+        //     src1R();
+        //     src2R();
+        //     break;
     }
 }
 
