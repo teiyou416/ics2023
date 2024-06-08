b1bfb90516aa8384c786d38ebf5f6c81934ca073
tracer-ics2023 <tracer@njuics.org>
2024-06-08 23:04:35 +0900

    >  compile NEMU 221220000 张三 Linux teiyou 6.5.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 26 11:23:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:04:35 up 1 day,  8:17,  2 users,  load average: 1.28, 1.40, 1.38

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -24,11 +24,6 @@ static const uint32_t img[] = {
     0x0102c503, // lbu a0,16(t0)
     0x00100073, // ebreak (used as nemu_trap)
     0xdeadbeef, // some data
-    0x800002b7, // lui t0,0x80000
-    0x0002a023, // sw  zero,0(t0)
-    0x0002a503, // lw  a0,0(t0)
-    0x00100073, // ebreak (used as nemu_trap)
-    0x00000413, // li
 };
 
 static void restart() {
