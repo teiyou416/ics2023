ee0ee20a3cd4a0025338ec4abb1da3f18ac00c31
chengye <teiyou>
2024-06-11 23:06:43 +0000

    vsprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,37 +4,20 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-static char sprint_buf[1024];
-int printf(const char *fmt, ...) {
-    va_list args;
-    int n;
-    va_start(args, fmt);
-    n = vsprintf(sprint_buf, fmt, args);
-    va_end(args);
-    putstr(sprint_buf);
-    return n;
-}
+static *num(char *str, int num, int base) { char * }
+int printf(const char *fmt, ...) {}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-    int len = vsnprintf(out, sizeof(sprint_buf), fmt, ap);
-    return len;
-}
+    //}
 
-int sprintf(char *out, const char *fmt, ...) { // 记录fmt对应的地址
-    va_list args;
-    int val;
-    va_start(args, fmt);
-    val = vsprintf(sprint_buf, fmt, args);
-    va_end(args);
-    return val;
-}
+    int sprintf(char *out, const char *fmt, ...) {}
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-    panic("Not implemented");
-}
+    int snprintf(char *out, size_t n, const char *fmt, ...) {
+        panic("Not implemented");
+    }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-    panic("Not implemented");
-}
+    int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+        panic("Not implemented");
+    }
 
 #endif
